0e852fe87cd3af04f40489a91440bfcb0b2bf618
pnts <pnts@protocol7.net>
2010-09-21 16:56:22 -0400

    archive stye fix, search button alignment, and character encoding fix

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-  <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>" />
+  <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
   <title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
   
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
