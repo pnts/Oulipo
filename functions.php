72e3ab13146a059004f280f454bc924ed7d7dee6
pnts <pnts@protocol7.net>
2010-10-12 21:57:19 -0400

    preview styles for admin view

--- functions.php
+++ functions.php
@@ -25,13 +25,39 @@ if ( function_exists( 'add_theme_support' )) {
     
   function oulipo_admin_header_style() { ?>
     <style type="text/css">
-            #headimg { background: url(<?php header_image() ?>) no-repeat;
-                       width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
-                       height: <?php echo HEADER_IMAGE_HEIGHT; ?>px; 
+            #headimg { 
                       }
     </style><?php }
+    
+    function oulipo_admin_header_image() { ?>
+       <div id="headimg">
+         <style type="text/css">
+            h1.masthead, p.description { font-family: "Hoefler Text","Cambria",Georgia,"Times New Roman",Times,serif; }
+            p.description { color: #666666; font-size: 13px; }
+            h1.masthead a { font-size: 24px; font-weight: normal; font-variant: small-caps; text-decoration: none; }
+            h1.masthead { margin: 30px 0 0 0;}
+         </style>
+           <?php
+           if ( 'blank' == get_theme_mod( 'header_textcolor',
+    HEADER_TEXTCOLOR ) || '' == get_theme_mod( 'header_textcolor',
+    HEADER_TEXTCOLOR ) )
+               $style = ' style="display:none;"';
+           else
+               $style = ' style="color:#' . get_theme_mod(
+    'header_textcolor', HEADER_TEXTCOLOR ) . ';"';
+           ?>
+           <img src="<?php esc_url ( header_image() ); ?>" alt="" />
+           
+           <h1 class="masthead"><a <?php echo $style; ?> onclick="return false;"
+    href="<?php bloginfo( 'url' ); ?>"><?php bloginfo( 'name' );
+    ?></a></h1>
+           <p class="description"><?php bloginfo(
+    'description' ); ?></div>
+           
+       </div>
+    <?php }   
   
-  add_custom_image_header('oulipo_header_style','oulipo_admin_header_style');
+  add_custom_image_header('oulipo_header_style','oulipo_admin_header_style', 'oulipo_admin_header_image');
   
   add_action( 'init', 'register_oulipo_menus' );
   function register_oulipo_menus() {
