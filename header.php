a3efab2e1d5f4e60fff3420bcd2e1c2a94f2dd1b
pnts <pnts@protocol7.net>
2010-10-18 20:25:01 -0400

    proper language and XFN attributes added to the header

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head>
+  <head profile="http://gmpg.org/xfn/11">
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
   <title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
-  
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
   <link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php bloginfo('url'); ?>/xmlrpc.php?rsd" />
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
